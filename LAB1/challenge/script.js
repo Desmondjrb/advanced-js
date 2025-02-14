6c7aeaa2c0401443ac650e9cbef719f5e597edf6
Desmondjrb <desmondjrb@nycstudents.net>
2025-02-14 18:18:47 +0000

    changes

--- LAB1/challenge/script.js
+++ LAB1/challenge/script.js
@@ -174,16 +174,19 @@ console.log(buyMeals(18))
 
 const missingLeg = (side1, side2) => {
     if ( side1 > side2){
-      
+      side1 = side1* side1
+      side2 = side2 * side2
         let leg2 = Math.sqrt(side1-side2)
         return " the missing leg is " + leg2
     } else {
+        side1 = side1 * side1;
+        side2 = side2 * side2;
         leg2 = Math.sqrt(side2-side1)
         return " the missing leg is " + leg2;
         
     }
 }
-console.log(missingLeg(18, 20))
+console.log(missingLeg(20, 18))
 // 18. [Super Challenge] In math, Factorial is the product of an integer and all the integers below it.
 /// For example: factorial(3) = 3*2*1 = 6 ---  factorial(5) = 5*4*3*2*1 = 120
 /// Write a function factorial(x) that finds the factorial of any integer.
